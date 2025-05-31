f5da930cd811b8c34e8a506d0021142e616408d0
Volkiw <laurawlobos@gmail.com>
2025-05-31 14:09:44 +0200

    avances js

--- src/js/main.js
+++ src/js/main.js
@@ -16,16 +16,31 @@ const cardWeapon = document.querySelector(".js-card-weapon");
 const inputWeapon = document.querySelector(".js-input-weapon");
 const cardOrigin = document.querySelector(".js-card-origin");
 const inputOrigin = document.querySelector(".js-input-origin");
+const cardSkill = document.querySelector(".js-card-skill");
+const inputSkill = document.querySelector(".js-input-skill");
+const cardFamily = document.querySelector(".js-card-family");
+const inputFamily = document.querySelector(".js-input-family");
 
 
 
 function handleClickWeapon(event) {
     event.preventDefault();
     let weapon = event.target.value;
-    console.log(weapon);
     cardWeapon.innerHTML = weapon;
-}
+};
+
+function handleClickOrigin(event) {
+    event.preventDefault();
+    let origin = event.target.value;
+    cardOrigin.innerHTML = origin;
+};
 
 
 
-inputWeapon.addEventListener("click", handleClickWeapon);
+
+
+
+
+inputWeapon.addEventListener("change", handleClickWeapon); // select va con change como input va con input 
+inputOrigin.addEventListener("change", handleClickOrigin);
+
