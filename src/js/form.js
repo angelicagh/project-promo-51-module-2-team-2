37031a4f3c97203b61f3d5f269867532183a3d20
Marina Boyano <marina.boyano.gonzalez@gmail.com>
2025-06-05 20:02:15 +0200

    reset

--- src/js/form.js
+++ src/js/form.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 console.log("form funciona");
 let inputSelectAll = document.querySelectorAll(".js_input");
@@ -8,12 +8,11 @@ const inputLevel = document.querySelector(".js-input-level ");
 const cardLevel = document.querySelector(".js-card-level");
 const selectBg = document.querySelectorAll(".js_background-img");
 const card = document.querySelector(".js-card");
-//CÓDIGO INICIAL::: 
+//CÓDIGO INICIAL:::
 //const cardWeapon = document.querySelector(".js-card-weapon");
 //const inputWeapon = document.querySelector(".js-input-weapon");
 //const cardOrigin = document.querySelector(".js-card-origin");
 //const inputOrigin = document.querySelector(".js-input-origin");
-
 
 /* function handleClickWeapon(event) {
     event.preventDefault();
@@ -55,90 +54,116 @@ inputOrigin.addEventListener("change", handleClickOrigin);
     } 
 }; */
 
-
 // FUNCIÓN MEGA RESUMIDA::::
 function handleInput(ev) {
-    let item = ev.target.value;
-    let input = ev.currentTarget.id;
-    let cardElClass = `.js-card-${input}`;
-    let cardEl = document.querySelector(cardElClass);
-    cardEl.innerHTML = item;
-};
-
+  let item = ev.target.value;
+  let input = ev.currentTarget.id;
+  let cardElClass = `.js-card-${input}`;
+  let cardEl = document.querySelector(cardElClass);
+  cardEl.innerHTML = item;
+}
 
 function initInputs() {
-    inputSelectAll.forEach((inputSelect) => {
-        inputSelect.addEventListener("input", handleInput);
-    });
-};
+  inputSelectAll.forEach((inputSelect) => {
+    inputSelect.addEventListener("input", handleInput);
+  });
+}
 
 initInputs();
 
 // input número
 
-function handleClickLevel (event) {
-    event.preventDefault();
-    let level = parseInt(inputLevel.value);
-    if (level > 100 || level < 1) {
-        //alert("Por favor, ingresa un número entre 1 y 100");//sale un mensaje de alerta para que añada un numero valido y no se escribe en la tarjeta
-        inputLevel.value = "";//tras el mensaje de alerta el campo del nivel se vuelve a quedar en blanco
-        inputLevel.placeholder = "Tiene que ser menos de 100";
-    } else {
-        cardLevel.innerHTML = level;
-    }  
-};
-
-
+function handleClickLevel(event) {
+  event.preventDefault();
+  let level = parseInt(inputLevel.value);
+  if (level > 100 || level < 1) {
+    //alert("Por favor, ingresa un número entre 1 y 100");//sale un mensaje de alerta para que añada un numero valido y no se escribe en la tarjeta
+    inputLevel.value = ""; //tras el mensaje de alerta el campo del nivel se vuelve a quedar en blanco
+    inputLevel.placeholder = "Tiene que ser menos de 100";
+  } else {
+    cardLevel.innerHTML = level;
+  }
+}
 
 // checkbox los 4 elementos
-checkboxElements.addEventListener ("change", (ev) => {
-    let element = ev.target;
-    let elValue = element.value;
-    let elClass = `.js-element-${elValue}`;
-    let elementByClass = document.querySelector(elClass);
-    if (element.checked === true){
-        elementByClass.classList.remove("hidden");
-    } else {
+checkboxElements.addEventListener("change", (ev) => {
+  let element = ev.target;
+  let elValue = element.value;
+  let elClass = `.js-element-${elValue}`;
+  let elementByClass = document.querySelector(elClass);
+  if (element.checked === true) {
+    elementByClass.classList.remove("hidden");
+  } else {
     elementByClass.classList.add("hidden");
-    }
-
+  }
 });
 
 // imagen fondo ponerla bonita:::
 
-function handleSelectBg(ev){
-    let background = ev.target;
-    let inputBgId = `input-${background.id}`;
-    let cardBgClass = `card-${background.id}`;
-    let inputBg = document.getElementById(inputBgId);
-    //console.log(background); 
-    if (inputBg.checked === false){
-        selectBg.forEach((bg) => {
-        bg.classList.remove("form-background__img--selected"); 
-        });
-       card.classList.remove("card-bg-1"); 
-              card.classList.remove("card-bg-2"); 
-       card.classList.remove("card-bg-3"); 
-       card.classList.remove("card-bg-4"); 
+function handleSelectBg(ev) {
+  let background = ev.target;
+  let inputBgId = `input-${background.id}`;
+  let cardBgClass = `card-${background.id}`;
+  let inputBg = document.getElementById(inputBgId);
+  //console.log(background);
+  if (inputBg.checked === false) {
+    selectBg.forEach((bg) => {
+      bg.classList.remove("form-background__img--selected");
+    });
+    card.classList.remove("card-bg-1");
+    card.classList.remove("card-bg-2");
+    card.classList.remove("card-bg-3");
+    card.classList.remove("card-bg-4");
 
-        inputBg.checked;
-        background.classList.add("form-background__img--selected"); 
-        card.classList.add(cardBgClass);
-    } else if (inputBg.checked === false){
-        background.classList.remove("form-background__img--selected");
-    
-    } 
-};
-
+    inputBg.checked;
+    background.classList.add("form-background__img--selected");
+    card.classList.add(cardBgClass);
+  } else if (inputBg.checked === false) {
+    background.classList.remove("form-background__img--selected");
+  }
+}
 
 function initSelectBg() {
-    selectBg.forEach((bg) => {
-        bg.addEventListener("click", handleSelectBg);
-    });
-};
+  selectBg.forEach((bg) => {
+    bg.addEventListener("click", handleSelectBg);
+  });
+}
 
 initSelectBg();
 
 // EVENTOS
 inputLevel.addEventListener("input", handleClickLevel);
+
+const resetButton = document.querySelector(".js-reset-btn");
+
+let name = document.querySelector(".js-card-name");
+let family = document.querySelector(".js-card-family");
+let description = document.querySelector(".js-card-description");
+let skill = document.querySelector(".js-card-skill");
+let origin = document.querySelector(".js-card-origin");
+let weapon = document.querySelector(".js-card-weapon");
+let level = document.querySelector(".js-card-level");
+let image = document.querySelector(".js-profile-image");
+let elementagua = document.querySelector(".js-element-agua");
+let elementfuego = document.querySelector(".js-element-fuego");
+let elementviento = document.querySelector(".js-element-viento");
+let elementtierra = document.querySelector(".js-element-tierra");
+function resetCard() {
+  level.innerHTML = "58";
+  name.innerHTML = "Éowyn, lady of Rohan";
+  family.innerHTML = "Humana";
+  description.innerHTML =
+    "Éowyn es un personaje de la novela fantástica El Señor de los Anillos, de J. R. R. Tolkien. Es una noble de Rohan.";
+  skill.innerHTML = "Rugido de núcleo";
+  origin.innerHTML = "Terragónida";
+  weapon.innerHTML = "Talismán";
+  image.style.backgroundImage = "url('./images/eowyn.jpg')";
+  elementagua.classList.add("hidden");
+  elementfuego.classList.add("hidden");
+  elementviento.classList.add("hidden");
+  elementtierra.classList.add("hidden");
+}
+
+resetButton.addEventListener("click", resetCard);
+
 
